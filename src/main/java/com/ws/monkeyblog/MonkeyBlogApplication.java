8bb48dce88271c2de65e1ff585ad5137da21de2f
TCsen <wangsen961225@163.com>
2020-05-31 12:36:21 +0800

    初始化

---
+++ src/main/java/com/ws/monkeyblog/MonkeyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.ws.monkeyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ws/monkeyblog/MonkeyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.monkeyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkeyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
